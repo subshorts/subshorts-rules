027c24bf6c569afe1d3b6079e9791dc7e2e190a3
Seokjin Yoon <sjy5386@gmail.com>
2022-03-20 18:17:22 +0900

    Update views.py

--- views.py
+++ views.py
@@ -1,8 +1,25 @@
-from flask import Blueprint
+import urllib.parse
+
+from flask import Blueprint, request, abort
+
+from models import PageRule
 
 bp = Blueprint('', __name__, url_prefix='/')
 
 
 @bp.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return view(None)
+
+
+@bp.route('/<path>')
+def view(path):
+    url = urllib.parse.urlparse(request.url)
+    page_rules = PageRule.query.filter(PageRule.domain == url.netloc).all()
+    if len(page_rules) > 0:
+        page_rule: PageRule = page_rules[0]
+        if page_rule.forwarding_domain:
+            return 'forwarding'
+        elif page_rule.parking_title:
+            return 'parking'
+    abort(404)
