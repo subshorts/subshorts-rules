5c1d33ef43c76f19e588528c59d0cc04247ef18c
Seokjin Yoon <sjy5386@gmail.com>
2022-03-20 15:02:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route('/')
+    def hello_world():  # put application's code here
+        return 'Hello World!'
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
